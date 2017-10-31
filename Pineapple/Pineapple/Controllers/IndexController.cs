f57d8e3b2b672b5844ad0745934cc121d1349a81
Anastasiia Horban <horban.nastya@gmail.com>
2017-10-31 10:24:18 +0200

    Add files via upload
    initial commit

---
+++ Pineapple/Pineapple/Controllers/IndexController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pineapple.Controllers
+{
+    [Route("/")]
+    public class IndexController : Controller
+    {
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Pineapple/Pineapple/Controllers/TweetsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pineapple.Controllers
+{
+    [Route("api/[controller]")]
+    public class TweetsController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "sweet1", "sweet2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "some swwwettt";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
