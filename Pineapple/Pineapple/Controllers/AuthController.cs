f0c436d09c2982531b5b1a57b5f368a1d3c3be33
Danial Shlenov <otetsdiodor@gmail.com>
2017-11-21 11:31:29 +0200

    Переделал метод Login. Добавил 2 модели LRM и User https://github.com/horba/pineapple/issues/16

---
+++ Pineapple/Pineapple/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Pineapple.Model;
+
+namespace Pineapple.Controllers
+{
+    public class AuthController :Controller
+    {
+        public LoginResponseModel Login(User currentUser)
+        {
+            if (currentUser.UserName == "test")
+            {
+                if (currentUser.Password == "Passw0rd")
+                {
+                    return new LoginResponseModel("succes", "null");
+                }
+                else
+                {
+                    return new LoginResponseModel("failure", "Wrong password");
+                }
+            }
+            else
+            {
+                return new LoginResponseModel("failure", "Wrong user");
+            }
+        }
+        public IActionResult Register()
+        {
+            return new EmptyResult();
+        }
+    }
+}
---
+++ Pineapple/Pineapple/Model/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pineapple.Model
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public User(string userName, string password)
+        {
+            UserName = userName;
+            Password = password;
+        }
+    }
+}
