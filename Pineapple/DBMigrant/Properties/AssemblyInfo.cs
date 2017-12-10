6dc5b745ac3af18f10cf850e2c85df4513b7ee38
Max Mozgovoj <maxmozgovoj13696@gmail.com>
2017-12-10 23:04:58 +0200

    Теперь программа работает на классах. Реализован откат миграций (на 1, при каждом запуске).

--- Pineapple/DBMigrant/Properties/AssemblyInfo.cs
+++ Pineapple/DBMigrant/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("DBMigrant")]
+[assembly: AssemblyTitle("DBMigration")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DBMigrant")]
+[assembly: AssemblyProduct("DBMigration")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
