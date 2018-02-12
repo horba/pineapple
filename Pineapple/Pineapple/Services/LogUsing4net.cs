818141d854939170e6928552a8a755d2c96d286b
OllyV <voa77777@gmail.com>
2018-02-12 21:35:28 +0200

    Added Logger based on Log4Net

---
+++ Pineapple/Pineapple/Services/LogUsing4net.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Pineapple.Model;
+using Pineapple.DBServices;
+using System.Data.SqlClient;
+using log4net;
+using System.Reflection;
+using log4net.Config;
+
+namespace Pineapple.Services
+{
+    public class LogUsing4net : ILogG
+    {
+
+        private readonly log4net.ILog log;
+
+        public LogUsing4net() {this.log = log4net.LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+    }
+
+        public void WriteInfo(string text)
+        { log.Info(text); }
+
+        public void WriteError(string text)
+        { log.Error(text); }
+
+        public void WriteWarning(string text)
+        { log.Warn(text); }
+
+        public void WriteDebug(string text)
+        { log.Debug(text); }
+
+        public void WriteFatal(string text)
+        { log.Fatal(text); }
+
+        public void WriteInfoExp(string text, Exception e)
+        {
+            log.Info(text,e);
+        }
+
+        public void WriteErrorExp(string text, Exception e)
+        {
+            log.Error(text, e);
+        }
+
+        public void WriteWarningExp(string text, Exception e)
+        {
+            log.Warn(text, e);
+        }
+
+        public void WriteDebugExp(string text, Exception e)
+        {
+            log.Debug(text, e);
+        }
+
+        public void WriteFatalExp(string text, Exception e)
+        {
+            log.Fatal(text, e);
+        }
+
+    }
+}
--- Pineapple/Pineapple/Startup.cs
+++ Pineapple/Pineapple/Startup.cs
@@ -9,13 +9,29 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Pineapple.Services;
 using Pineapple.Controllers;
+using log4net;
+using System.Reflection;
+using log4net.Config;
 
 namespace Pineapple
 {
     public class Startup
     {
+        //private static readonly log4net.ILog log = log4net.LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
         public Startup(IHostingEnvironment env)
         {
+            log4net.Config.XmlConfigurator.Configure();
+            
+            LogUsing4net _Log = new LogUsing4net(); 
+            _Log.WriteWarning("Some text");
+            _Log.WriteInfo("Some text");
+            _Log.WriteFatal("Some text");
+            _Log.WriteDebug("Some text");
+            _Log.WriteError("Some text");
+             _Log.WriteDebugExp("Some text",new Exception("Exception"));
+
+
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
@@ -32,6 +48,8 @@ namespace Pineapple
             // Add framework services.
             services.AddMvc();
             services.AddTransient<IUserAuth, UserAuth>();
+            services.AddTransient<ILogG, LogUsing4net>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
